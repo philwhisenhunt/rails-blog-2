a7cc58ed33366be301033f228efbcf5f230e9e08
Philip Whisenhunt <philip.whisenhunt@gmail.com>
2020-02-21 11:05:17 -0500

    Checking in before heroku deploy 2

--- test/models/high_score_test.rb
+++ test/models/high_score_test.rb
@@ -1,5 +1,8 @@
+# Requiring this file allows us to load 
+# the default configuration for our tests
 require 'test_helper'
 
+#HighScoreTest inherits from the ActiveSupport::TestCase and has access to all to the methods it does. 
 class HighScoreTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
